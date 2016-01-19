ebe346573bcad49bc8664f6d8a64bcfe19a763dd
Scott Humphries <git@sscotth.io>
2016-01-19 14:54:29 -0600

    add range tests

--- tests/test.js
+++ tests/test.js
@@ -24,3 +24,29 @@ describe('helloWorld', function () {
     expect(hello()).to.equal('world');
   });
 });
+
+describe('utility functions', function () {
+  describe('range', function () {
+    var range = require('../range.js');
+
+    it('range should output an array', function () {
+      expect(range()).to.be.an('array');
+    });
+
+    it('array be the length passed as the argument', function () {
+      expect(range()).to.have.length(0);
+      expect(range(1)).to.have.length(1);
+      expect(range(100)).to.have.length(100);
+    });
+
+    it('should return a array range', function () {
+      expect(range(3)).to.eql([0,1,2]);
+      expect(range(5)).to.eql([0,1,2,3,4]);
+    });
+
+    it('should accept a start argument', function () {
+      expect(range(3, 5)).to.eql([3,4]);
+      expect(range(4, 9)).to.eql([4,5,6,7,8]);
+    });
+  });
+});
